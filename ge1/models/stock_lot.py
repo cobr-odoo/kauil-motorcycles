7c97b6dda00c17e3bd38e5d132ba21561b5fd9c5
cobr-odoo <cobr@odoo.com>
2023-06-21 15:00:53 -0400

    corey's branch

--- ge1/models/stock_lot.py
+++ ge1/models/stock_lot.py
@@ -5,11 +5,17 @@ class StockLot(models.Model):
 
 
     def _get_next_serial(self, company, product):
+
+
         prod_tmpl_id = product.product_tmpl_id
         if prod_tmpl_id.detailed_type == 'motorcycle' and product.tracking != 'none':
-            if prod_tmpl_id.make and prod_tmpl_id.model and prod_tmpl_id.year:
-                vin_part = prod_tmpl_id.make + prod_tmpl_id.model + str(prod_tmpl_id.year)[-2]
-                return vin_part + self.env['ir.sequence'].next_by_code('stock.lot.serial')
+
+            prod_make = prod_tmpl_id.make if prod_tmpl_id.make else 'XX'
+            prod_model = prod_tmpl_id.model if prod_tmpl_id.model else 'XX'
+            prod_year = str(prod_tmpl_id.year)[-2:] if prod_tmpl_id.year else '00'
+            sequence = self.env['ir.sequence'].next_by_code('stock.lot.serial')
+            return f'{prod_make}{prod_model}{prod_year}{sequence}'
+
         else:
             return super(StockLot, self)._get_next_serial(company, product)
         
