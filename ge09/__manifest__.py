46d822de4c950a169c7642846d119fd131cf561b
cobr-odoo <cobr@odoo.com>
2023-06-26 11:59:02 -0400

    init and manifest added

---
+++ ge09/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "GE09",
+    
+    "summary": "",
+    
+    "description": """
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": [],
+    
+    "data": [
+     
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
