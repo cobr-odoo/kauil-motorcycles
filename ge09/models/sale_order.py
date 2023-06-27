b06016921701bde9c4c4fa325589d1330fd09bf8
cobr-odoo <cobr@odoo.com>
2023-06-27 12:57:01 -0400

    commit for jason

--- ge09/models/sale_order.py
+++ ge09/models/sale_order.py
@@ -3,9 +3,19 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    new_customer = fields.Boolean(compute='_compute_new_customer', default=False)
+    new_customer = fields.Boolean(compute='_compute_new_customer', default=True)
 
     @api.depends('partner_id')
     def _compute_new_customer(self):
-        for record in self:
-            record.new_customer = True
+        pass
+
+    #     for record in self:
+            
+    #         record.new_customer = True
+    #         for orders in record.partner_id.sale_order_ids[1:]:
+    #             if orders.order_line.product_id.detailed_type == 'motorcycle':
+    #                 record.new_customer = False
+    #                 break
+                
+
+                
